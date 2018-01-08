97932faee9ff3d45eb34dc4dc45f0c0864dd98a9
ptspzy <ptspzy@126.com>
2018-01-08 18:43:40 +0800

    changed: axios 请求数据

---
+++ src/pages/RequestDataTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    {{cityData}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'RequestDataTest',
+  data () {
+    return {
+      cityData: {}
+    }
+  },
+  async created () {
+    const { data: { data } } = await axios.get('https://easy-mock.com/mock/5a5215d2d408383e0e385e64/test/city')
+    this.cityData = data
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import RequestDataTest from '@/pages/RequestDataTest'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/test001',
+      name: 'RequestDataTest',
+      component: RequestDataTest
     }
   ]
 })
