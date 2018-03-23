96bbd7bf7d86dc679304b294147b2bc7b7ac5f90
ptspzy <ptspzy@126.com>
2018-03-23 23:32:47 +0800

    feat: demo001

---
+++ src/pages/demo001/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="button" value="显示\隐藏" v-on:click="switchShowStatus">
+    <div v-if="isShow" class="box">v-if</div>
+    <div v-show="isShow" class="box">v-show</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo001',
+  data () {
+    return {
+      isShow: true
+    }
+  },
+  methods: {
+    switchShowStatus () {
+      this.isShow = !this.isShow
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .box {
+   width: 100px;
+   height: 100px;
+   background-color: bisque;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 10px;
+ }
+</style>
+
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import RequestDataTest from '@/pages/RequestDataTest'
+import demo001 from '@/pages/demo001'
 
 Vue.use(Router)
 
@@ -11,10 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }, {
-      path: '/test001',
+    },
+    {
+      path: '/demo002',
       name: 'RequestDataTest',
       component: RequestDataTest
+    },
+    {
+      path: '/demo001',
+      name: 'demo001',
+      component: demo001
     }
   ]
 })
