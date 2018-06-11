080001ba87336adc10bc9817b8adfcb9c825e54a
ptspzy <ptspzy@126.com>
2018-06-11 14:16:58 +0800

    temp

---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,34 @@
+<template>
+   <l-map class="map" ref="map" :zoom=13 :center="[47.413220, -1.219482]">
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+export default {
+  name: 'example',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      // zoom: 13,
+      // center: L.latLng(47.413220, -1.219482),
+      // url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      // marker: L.latLng(47.413220, -1.219482),
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .map {
+   width: 1000px;
+   height: 1000px;
+ }
+</style>
+
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <debounce-search/>
+    <leaflet-map/>
   </div>
 </template>
 <script>
 import DebounceSearch from '@/components/DebounceSearch'
+import LeafletMap from '@/components/LeafletMap'
 export default {
   name: 'index',
   components: {
-    DebounceSearch
+    DebounceSearch,
+    LeafletMap
   }
 }
 </script>
