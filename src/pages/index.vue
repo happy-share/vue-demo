cc14c51bdca192770ddd207721d83a6694ef9250
ptspzy <ptspzy@126.com>
2018-06-23 11:12:15 +0800

    vue.set

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,7 @@
     <wired-listbox>
       <wired-item value="wired-elements" text="wired-elements" v-on:click="jump('/wired-elements')"></wired-item>
     </wired-listbox>
+    <h1 @click="handle">click</h1>
     <debounce-search/>
     <tool-countdown
       :countdown="countdown"
@@ -14,11 +15,12 @@
     >
     </tool-countdown>
     <test-list
-      :list="[1,2,3,4,5,6,7]"
+      :list=list
     ></test-list>
   </div>
 </template>
 <script>
+import Vue from 'vue'
 import DebounceSearch from '@/components/DebounceSearch'
 import ToolCountdown from '@/components/tool-countdown'
 import TestList from '@/components/test-list'
@@ -35,7 +37,8 @@ export default {
     return {
       countdown: 10,
       numStyle: 'width: 48rpx; font-size: 28rpx; color: #ffffff; background: #000; text-align: center; border-radius: 8rpx; padding: 5rpx 0;',
-      symbolStyle: 'font-size: 28rpx; color: #000; padding: 0 12rpx;'
+      symbolStyle: 'font-size: 28rpx; color: #000; padding: 0 12rpx;',
+      list: [1, 2, 3, 4, 5, 6, 7]
     }
   },
   components: {
@@ -52,10 +55,25 @@ export default {
     },
     jump (url) {
       this.$router.push(url)
+    },
+    handle () {
+      // this.list[0] = 1000 // 数组元素更改 不是响应式 同理 对象也是（除非对象属性显示申明，新添的属性没有响应式）
+
+      // // 解决1 复制
+      // const newList = [...this.list]
+      // newList[0] = 1000
+      // this.list = newList
+
+      // 解决2
+      Vue.set(this.list, 0, 1000)
+
+      console.log(this.list, 111)
     }
   },
   created () {
     console.log(1, utils)
+  },
+  mounted () {
   }
 }
 </script>
